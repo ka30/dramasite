0492ba3786a402c4709ff7cfd710275cdeb7626a
ka30 <el.7oob1@gmail.com>
2013-08-16 08:32:12 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dramasite::Application.config.secret_key_base = '75cc0d0981842973158fc183c295a47c58c05e8ba4ef5bf2d8406cd634da03e672c525cb522aafaa3e89bf209c8eb6f3b215778933dd7c952799c43862982264'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dramasite::Application.config.session_store :cookie_store, key: '_dramasite_session'
